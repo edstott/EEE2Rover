d819f935ba60fcbb60bc3596485268969d25d94b
Ed <ed.stott@imperial.ac.uk>
2022-01-05 15:05:55 +0000

    Added POST data decode and servo control to ESP32

--- ESP32/WebTest/WebTest.ino
+++ ESP32/WebTest/WebTest.ino
@@ -2,13 +2,23 @@
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
+#include <ESP32Servo.h>
+#include <ArduinoJson.h>
 
-const char* ssid = "EEERover";
-const char* password = "exhibition";
+const char* ssid = "EE-p4h4nb";
+const char* password = "stop-serve-wet";
 
 WebServer server(80);
+DynamicJsonDocument doc(1024);
+
+Servo servoLeft;
+Servo servoRight;
+Servo servoLift;
 
 const int led = 13;
+
+int wcount = 0;
+int msgCount = 0;
 
 void handleRoot() {
   digitalWrite(led, 1);
@@ -34,10 +44,26 @@ void handleNotFound() {
 }
 
 void drive() {
-  if (server.args() > 0)
-    Serial.println(server.arg(0));
-  //server.send(200, "text/plain", "");
-  server.send(200);
+  if (server.args() > 0) {
+    server.send(204);
+    deserializeJson(doc,server.arg(0));
+    int packetNumber = doc["count"];
+    int X2 = doc["X2"];
+    int Y2 = doc["Y2"];
+    int Y1 = doc["Y1"];
+    if (packetNumber > msgCount || packetNumber == 0){
+      msgCount = packetNumber;
+      setMotors(X2,100-Y2,100-Y1);
+    }
+    if (packetNumber % 5 == 0){
+      Serial.print(packetNumber);
+      Serial.print(':');
+      Serial.print(' ');
+      Serial.println(server.arg(0));
+    }
+  } else {
+    server.send(400);
+  }
 }
 
 void setup(void) {
@@ -75,8 +101,30 @@ void setup(void) {
 
   server.begin();
   Serial.println("HTTP server started");
+
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  servoLeft.setPeriodHertz(50);
+  servoRight.setPeriodHertz(50);
+  servoLift.setPeriodHertz(50);
+  servoLeft.attach(23);
+  servoRight.attach(22);
+  servoLift.attach(21);
+  
+  Serial.println("Servos initialised");
 }
 
 void loop(void) {
   server.handleClient();
+}
+
+void setMotors(int X, int Y, int Z){
+  int left = (X < 50) ? Y : (Y * (100 - X) * 2) / 100;
+  int right = (X > 50) ? Y : (Y * X * 2) / 100;
+  int lift = Z;
+  servoLeft.write(left);
+  servoRight.write(right); 
+  servoLift.write(lift);  
 }
